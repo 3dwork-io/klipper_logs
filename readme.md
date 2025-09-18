c47c590b7a6e3716f9deb5821f943d713579bca1
JJR3D <jrodriguez@qitec.net>
2025-09-18 17:17:21 +0200

    Added readme and other title changes

---
+++ readme.md
@@ -0,0 +1,3 @@
+install aiohttp dependency - python3 -m pip install --user aiohttp
+install kconfiglib dependency - python3 -m pip install --user kconfiglib
+Access using :8998 port
--- server.py
+++ server.py
@@ -346,7 +346,7 @@ def process_logfile(digest, htmlfile):
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Klipper Log Parser</title>
+<title>3Dwork - Klipper Log Parser</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <style>
 div.code code::before {
@@ -1537,7 +1537,7 @@ async def handle_list(request: web.Request) -> web.StreamResponse:
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Klipper Log Parser</title>
+<title>3Dwork - Klipper Log Parser</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
